2eea94bf2135ad9b694ab84b96f1d2d76fda3fd7
Aditya4628 <120693555+Aditya4628@users.noreply.github.com>
2024-04-04 13:08:01 -0400

    Add files via upload

---
+++ COVID_Analysis_SQL.sql
@@ -0,0 +1,114 @@
+select * from CovidDeaths
+order by location, date asc
+
+select * from CovidVaccinations
+
+select * from CovidDeaths
+where continent is not null
+
+-- Data that we are going to be starting with
+Select Location, date, total_cases, new_cases, total_deaths, population
+from CovidDeaths
+where continent is not null
+order by iso_code, continent
+
+ --Total Cases vs Total Deaths
+ --Shows likelihood of dying if you contract covid in your country
+
+Select Location, date, total_cases,total_deaths, (total_deaths/total_cases)*100 as DeathPercentage
+From CovidDeaths
+Where location like '%states%'
+and continent is not null 
+order by 1,2
+
+-- Total Cases vs Population
+-- Shows what percentage of population infected with Covid
+Select Location, date, Population, total_cases,  (total_cases/population)*100 as PercentPopulationInfected
+From CovidDeaths
+Where location like '%states%'
+order by 1,2
+
+-- Countries with Highest Infection Rate compared to Population
+Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From CovidDeaths
+--Where location like '%states%'
+Group by Location, Population
+order by PercentPopulationInfected desc
+
+-- Countries with Highest Death Count per Population
+-- we use cast because Total_deaths is an nvarchar data type
+Select Location, MAX(cast(Total_deaths as int)) as TotalDeathCount
+From CovidDeaths
+--Where location like '%states%'
+Where continent is not null 
+Group by Location
+order by TotalDeathCount desc
+
+
+-- BREAKING THINGS DOWN BY CONTINENT
+
+-- Showing contintents with the highest death count per population
+
+Select location, MAX(cast(Total_deaths as int)) as TotalDeathCount
+From CovidDeaths
+--Where location like '%states%'
+Where continent is null 
+Group by location
+order by TotalDeathCount desc
+
+
+-- GLOBAL NUMBERS
+
+Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
+From CovidDeaths
+-- Where location like '%states%' 
+where continent is not null 
+order by 1,2
+
+
+-- Total Population vs Vaccinations
+-- Shows Percentage of Population that has recieved at least one Covid Vaccine
+
+select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations,
+sum(convert(int,vac.new_vaccinations)) over (partition by dea.location order by dea.location,
+dea.date) as rolling_people_vaccinated,
+--(rolling_people_vaccinated/population)*100
+from CovidDeaths dea
+join CovidVaccinations vac
+on dea.location = vac.location
+and dea.date = vac.date
+where dea.continent is not null
+order by 2,3
+
+-- Using a CTE as we can't just use a new column we just created
+
+with popvac (continent, location, date, population, new_vaccinations, rolling_people_vaccinated)
+as 
+(
+select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations,
+sum(convert(int,vac.new_vaccinations)) over (partition by dea.location order by dea.location,
+dea.date) as rolling_people_vaccinated
+--(rolling_people_vaccinated/population)*100
+from CovidDeaths dea
+join CovidVaccinations vac
+on dea.location = vac.location
+and dea.date = vac.date
+where dea.continent is not null
+--order by 2,3
+)
+select *, (rolling_people_vaccinated/population)*100 from popvac
+
+
+-- Creating View to store data for later visualizations
+
+Create View PercentPopulationVaccinated as
+Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
+, SUM(CONVERT(int,vac.new_vaccinations)) OVER (Partition by dea.Location Order by dea.location, dea.Date) as RollingPeopleVaccinated
+--, (RollingPeopleVaccinated/population)*100
+From CovidDeaths dea
+Join CovidVaccinations vac
+	On dea.location = vac.location
+	and dea.date = vac.date
+where dea.continent is not null
+
+select * from PercentPopulationVaccinated
